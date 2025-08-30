7ee31587cbea7a31863e18bafba5c65c38582510
gMeghana7 <139792212+gMeghana7@users.noreply.github.com>
2025-08-30 22:56:23 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/', // Change to root path
+  base: '/', // Updated to root path for Vercel
   plugins: [react()],
 })
